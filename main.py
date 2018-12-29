ba7d222c35da6ca85f144c858176eaeaad27aef7
kgtkr <kgtkr.jp@gmail.com>
2018-12-29 22:53:55 +0900

    色々改善

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ ck = os.environ["ck"]
 cs = os.environ["cs"]
 tk = os.environ["tk"]
 ts = os.environ["ts"]
+sn = os.environ["sn"]
 
 # 認証
 auth = tweepy.OAuthHandler(ck, cs)
 auth.set_access_token(tk, ts)
 api = tweepy.API(auth)
 
-for t in tweepy.Cursor(api.favorites, screen_name="tayo1325", count=200).items():
+f = open("output.txt", "w")
+
+for t in tweepy.Cursor(api.favorites, screen_name=sn, count=200).items():
     if hasattr(t, "extended_entities"):
         for m in t.extended_entities["media"]:
             if m["type"] == "photo":
-                print(m["media_url_https"])
+                f.write(m["media_url_https"]+"\n")
+
+f.close()
